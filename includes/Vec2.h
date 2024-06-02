64dcd4ddf6874796a8d7c67c391812eca9abf413
Aaron William Po <apo2@uwo.ca>
2024-06-02 15:30:22 -0400

    Add Vec2 class

---
+++ includes/Vec2.h
@@ -0,0 +1,26 @@
+#ifndef VEC2_H
+#define VEC2_H
+
+class Vec2 {
+ public:
+  double x, y;
+
+  Vec2(float x = 0, float y = 0);
+
+  bool operator==(const Vec2 &rhs) const;
+  bool operator!=(const Vec2 &rhs) const;
+
+  Vec2 operator-(const Vec2 &rhs) const;
+  Vec2 operator*(const float val) const;
+  Vec2 operator/(const float val) const;
+
+  void operator+=(const Vec2 &rhs);
+  void operator-=(const Vec2 &rhs);
+  void operator*=(const float val);
+  void operator/=(const float val);
+
+  void normalize();
+  float length() const;
+};
+
+#endif  // VEC2_H
---
+++ src/Vec2.cpp
@@ -0,0 +1,57 @@
+#include "../includes/Vec2.h"
+#include <cmath>
+
+Vec2::Vec2(float x, float y) :
+    x(x), y(y) {}
+
+bool Vec2::operator==(const Vec2 &rhs) const {
+  return rhs.x == x && rhs.y == y;
+}
+
+bool Vec2::operator!=(const Vec2 &rhs) const {
+  return !(*this == rhs);
+}
+
+Vec2 Vec2::operator-(const Vec2 &rhs) const {
+  return Vec2(x - rhs.x, y - rhs.y);
+}
+
+Vec2 Vec2::operator*(const float val) const {
+  return Vec2(x * val, y * val);
+}
+
+Vec2 Vec2::operator/(const float val) const {
+  return Vec2(x / val, y / val);
+}
+
+void Vec2::operator+=(const Vec2 &rhs) {
+  x += rhs.x;
+  y += rhs.y;
+}
+
+void Vec2::operator-=(const Vec2 &rhs) {
+  x -= rhs.x;
+  y -= rhs.y;
+}
+
+void Vec2::operator*=(const float val) {
+  x *= val;
+  y *= val;
+}
+
+void Vec2::operator/=(const float val) {
+  x /= val;
+  y /= val;
+}
+
+float Vec2::length() const {
+  return std::sqrt(x * x + y * y);
+}
+
+void Vec2::normalize() {
+  float len = length();
+  if (len > 0) { // Avoid division by zero
+    x /= len;
+    y /= len;
+  }
+}
