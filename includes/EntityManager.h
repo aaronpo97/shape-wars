96d59d7cb548de25dcf36b60c03a888a9f8015d9
Aaron William Po <apo2@uwo.ca>
2024-06-06 21:59:17 -0400

    Edit comments in EntityManager header

--- includes/EntityManager.h
+++ includes/EntityManager.h
@@ -4,35 +4,24 @@
 #include "Entity.h"
 #include <memory>
 
-// store all entity objects in a vector
-
-// define a type alias for a vector of shared pointers to Entity objects
+// Store all entity objects in a vector.
 typedef std::vector<std::shared_ptr<Entity>> EntityVector;
 
-// also store separate vectors of Entity objects by their tag for quick retrieval
-
-// define a type alias for a map of strings to vectors of shared pointers to Entity
-// objects
-
-// example usage - "player", vector of all the player entities
+// Store separate vectors of Entity objects by their tag for quick retrieval.
 typedef std::map<std::string, EntityVector> EntityMap;
 
 class EntityManager {
   EntityVector m_entities;
   EntityVector m_toAdd;
   EntityMap    m_entityMap;
-  /*
-   *    `m_totalEntities` is not the amount of entities that are alive, but rather the
-   *    total amount of entities that are created.
-   */
-  size_t m_totalEntities = 0;
+  size_t       m_totalEntities = 0;
 
 public:
   EntityManager();
-  void                    update();
   std::shared_ptr<Entity> addEntity(const std::string &tag);
   EntityVector           &getEntities();
   EntityVector           &getEntities(const std::string &tag);
+  void                    update();
 };
 
 #endif // ENTITY_MANAGER_H
